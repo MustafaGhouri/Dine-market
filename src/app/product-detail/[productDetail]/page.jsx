adfc6dc39f284474fea8a77135ccdd514c7b6c53
MustafaGhouri <112586426+MustafaGhouri@users.noreply.github.com>
2023-07-06 16:57:05 +0500

    product detail param issue fix

---
+++ src/app/product-detail/[productDetail]/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react'
+
+const productDetails = (param) => {
+    console.log("param ",param?.params?.productDetail);
+  return (
+    <div>productDetails</div>
+  )
+}
+
+export default productDetails
---
+++ src/app/product-detail/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const productDetail = () => {
+  return (
+    <h1>productDetailww</h1>
+  )
+}
+
+export default productDetail
